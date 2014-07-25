b6192a04492415962b1eaec7a4fd8b5143e5a34e
Torben Höhn <torhoehn@gmail.com>
2014-07-25 09:47:11 +0200

    Doctrine & Zend Developer Tools

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -128,5 +128,19 @@ return array(
 				'action' => 'login'
 			)
 		)
-    )
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            'application_entities' => array(
+              'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+              'cache' => 'array',
+              'paths' => array(__DIR__ . '/../src/Application/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Application\Entity' => 'application_entities'
+                ),
+            )
+        )
+    ),
 );
